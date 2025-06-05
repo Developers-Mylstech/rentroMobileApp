dfcfca0e543fb252399643a792dd7245cc1a27e5
User Yaseen <mohdyaseen7610@gmail.com>
2025-06-05 14:03:14 +0530

    chnages

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,29 @@
+// const { getDefaultConfig } = require("expo/metro-config");
+// const { withNativeWind } = require('nativewind/metro');
+ 
+// const config = getDefaultConfig(__dirname)
+ 
+// module.exports = withNativeWind(config, { input: './global.css' })
+
+
 const { getDefaultConfig } = require("expo/metro-config");
 const { withNativeWind } = require('nativewind/metro');
- 
-const config = getDefaultConfig(__dirname)
- 
-module.exports = withNativeWind(config, { input: './global.css' })
+
+
+const config = getDefaultConfig(__dirname);
+
+// Optional: lightningcss transformer config
+config.transformer = {
+  ...config.transformer,
+  experimentalImportSupport: false,
+  unstable_disableES6Transforms: false,
+  getTransformOptions: async () => ({
+    transform: {
+      experimentalImportSupport: false,
+      inlineRequires: true,
+    },
+  }),
+};
+
+module.exports = withNativeWind(config, { input: './global.css' });
+
